304ca6aec8c8983584b331949d5fd6e190fcc655
Dima Shalupnya <dshalupnya@gmail.com>
2025-09-15 19:52:10 +0200

    update of html

--- app_spending/routes.py
+++ app_spending/routes.py
@@ -3,7 +3,13 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    datos=[
+        {"Date":"12/09/25", "Transaction": "Compra", "Amount": "129.90"},
+        {"Date":"13/09/25", "Transaction": "Compra", "Amount": "29.90"},
+        {"Date":"14/09/25", "Transaction": "Compra", "Amount": "9.90"}
+
+    ]
+    return render_template("index.html", data = datos)
 
 @app.route("/new")
 def new():
