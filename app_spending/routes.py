221967bd9565b872d4fd41666d890a07cb9b74b8
Dima Shalupnya <dshalupnya@gmail.com>
2025-09-16 19:51:55 +0200

    Añadimos los metodos post y get

--- app_spending/routes.py
+++ app_spending/routes.py
@@ -1,3 +1,4 @@
+"""
 from app_spending import app
 from flask import render_template
 
@@ -23,3 +24,43 @@ def update():
 @app.route("/delete")
 def delete():
     return render_template("delete.html")
+"""
+
+
+from app_spending import app
+from flask import render_template, request
+import csv
+
+@app.route("/")
+def index():
+    datos=[]
+    #llamada al archivo csv
+    fichero = open('data/movimientos.csv','r')
+    csvReader = csv.reader(fichero,delimiter=",",quotechar='"')
+    for items in csvReader:
+        datos.append(items)
+    """
+    datos=[
+        {'fecha':'01/09/2025','concepto':'Salario','monto':1500},
+        {'fecha':'05/09/2025','concepto':'Ropa','monto':-150},
+        {'fecha':'10/09/2025','concepto':'Supermercado','monto':-200}
+    ]
+    """
+    return render_template("index.html",data = datos,titulo="Lista")
+
+
+@app.route("/new",methods=["GET","POST"])
+def new():
+    if request.method == "POST":
+        return f"Esto deberia registrarse {request.form}"
+    else:
+        return render_template("new.html",titulo="Nuevo",tipoAccion="Registro",tipoBoton="Guardar")
+
+
+@app.route("/delete")
+def delete():
+    return render_template("delete.html",titulo="Borrar")
+
+@app.route("/update")
+def update():
+    return render_template("update.html",titulo="Actualizar",tipoAccion="Actualización",tipoBoton="Editar")
