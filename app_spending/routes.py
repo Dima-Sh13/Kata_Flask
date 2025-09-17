a3d4b67093de822f77b390433dedaac81d8e394c
Dima Shalupnya <dshalupnya@gmail.com>
2025-09-17 21:59:22 +0200

    Update of routes

--- app_spending/routes.py
+++ app_spending/routes.py
@@ -50,57 +50,6 @@ def update():
 def delete():
     return render_template("delete.html")
 
-
-
-
-"""
-
-
-from app_spending import app
-from flask import render_template, request, redirect
-import csv
-from datetime import date
-
-@app.route("/")
-def index():
-    datos=[]
-    #llamada al archivo csv
-    fichero = open('data/movimientos.csv','r')
-    csvReader = csv.reader(fichero,delimiter=",",quotechar='"')
-    for items in csvReader:
-        datos.append(items)
-    
-    datos=[
-        {'fecha':'01/09/2025','concepto':'Salario','monto':1500},
-        {'fecha':'05/09/2025','concepto':'Ropa','monto':-150},
-        {'fecha':'10/09/2025','concepto':'Supermercado','monto':-200}
-    ]
-    
-    return render_template("index.html",data = datos,titulo="Lista")
-
-
-@app.route("/new",methods=["GET","POST"])
-def new():
-    if request.method == "POST":
-        if validacion(request.form):
-            pass
-        mifichero = open("data/movimientos.csv", "a",newline="")
-        lectura = csv.writer(mifichero,delimiter=",",quotechar=" ")
-        mifichero.close()
-        return redirect("/")
-    else:
-        return render_template("new.html",titulo="Nuevo",tipoAccion="Registro",tipoBoton="Guardar")
-
-
-@app.route("/delete")
-def delete():
-    return render_template("delete.html",titulo="Borrar")
-
-@app.route("/update")
-def update():
-    return render_template("update.html",titulo="Actualizar",tipoAccion="Actualización",tipoBoton="Editar")
-"""   
-
 def validacion(datosFormulario):
     errores = []
     hoy = str(date.today())
