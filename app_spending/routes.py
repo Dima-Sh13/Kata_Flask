7c9ebac39ce9bb16658617a2f31105a512c778e1
Dima Shalupnya <dshalupnya@gmail.com>
2025-09-17 21:27:48 +0200

    Update of routes and new. Functionas cvs writing and id increment

--- app_spending/routes.py
+++ app_spending/routes.py
@@ -2,11 +2,13 @@
 from app_spending import app
 from flask import render_template, request
 import csv
-
+from datetime import date
+num = 1
+last_num = 0
 @app.route("/")
 def index():
     datos=[]
-    with open("../data/movimientos.csv", "r", encoding="utf-8") as f:
+    with open("data/movimientos.csv", "r", encoding="utf-8") as f:
         csvReader = csv.reader(f, delimiter=",", quotechar='"')
         for i in csvReader:
             datos.append(i)
@@ -14,11 +16,32 @@ def index():
 
 @app.route("/new", methods=["GET", "POST"])
 def new():
+    render = render_template("new.html")
+    ident = [0]
+    with open("data/movimientos.csv", "r", encoding="utf-8") as f:
+        csvReader = csv.reader(f, delimiter=",", quotechar='"')
+        
+        for i in csvReader:
+            ident.append(i[0])
+
+   
+    
     if request.method == "GET":
         return render_template("new.html")
     else:
-        print(request.form)
-
+        with open("data/movimientos.csv", "a", newline="", encoding="utf-8") as f:
+           
+               
+           
+            
+            writer = csv.writer(f)
+            if validacion(request.form) == True:
+                
+                writer.writerow([(int(ident[-1]) +1 or 0),request.form["Date"],request.form["Transaction"], request.form["Amount"]])
+                
+            else:
+               render = render_template("new.html", error = validacion(request.form))    
+    return render
 @app.route("/update")
 def update():
     return render_template("update.html")
@@ -26,6 +49,10 @@ def update():
 @app.route("/delete")
 def delete():
     return render_template("delete.html")
+
+
+
+
 """
 
 
@@ -72,7 +99,7 @@ def delete():
 @app.route("/update")
 def update():
     return render_template("update.html",titulo="Actualizar",tipoAccion="Actualización",tipoBoton="Editar")
-
+"""   
 
 def validacion(datosFormulario):
     errores = []
@@ -83,4 +110,8 @@ def validacion(datosFormulario):
         errores.append("Debe agregar un concepto de transaccion")
     if datosFormulario["Amount"]== "" or datosFormulario["Amount"] == 0:
         errores.append("Introduzca una cantidad valida")        
-"""        
+
+    if errores == []:
+        return True 
+    else:
+        return errores
