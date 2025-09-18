392ef27eca82dc836d1666610ee5fdbb5e0b11a3
Dima Shalupnya <dshalupnya@gmail.com>
2025-09-18 21:36:57 +0200

    Finished delete method

--- app_expenses/routes.py
+++ app_expenses/routes.py
@@ -108,7 +108,31 @@ def update(id):
 
 @app.route("/delete/<int:id>",  methods=["GET", "POST"])
 def delete(id):
-    return render_template("delete.html")
+    filas = []
+        # Primero leemos todas las filas del CSV
+    with open("data/movimientos.csv", "r", encoding="utf-8") as r:
+        csvReader = csv.reader(r, delimiter=",", quotechar='"')
+        for i in csvReader:
+            if i[0] != str(id):  # si es el registro a actualizar
+                filas.append(i)  
+               
+
+        with open("data/movimientos.csv", "w", newline="", encoding="utf-8") as w:
+            writer = csv.writer(w, delimiter=",", quotechar='"')
+            writer.writerows(filas)
+    
+    return redirect("/")
+    
+   
+            
+
+
+    
+
+
+
+
+
 
 def validacion(datosFormulario):
     errores = []
